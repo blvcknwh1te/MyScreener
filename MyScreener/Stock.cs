0436c14fb86b109578053bfff89eebc2884c65a4
verofeenko <erofeenko@bgs.by>
2023-08-14 17:02:46 +0300

    -

--- MyScreener/Stock.cs
+++ MyScreener/Stock.cs
@@ -11,28 +11,12 @@ namespace MyScreener
         public string Name { get; set; }
         public string Symbol { get; set; }
         public double Price { get; set; }
-        public double Capitalisation { get; set; }
+        public double Capitalization { get; set; }
         public int Volume { get; set; }
         public double Dividends { get; set; }
         public string Exchange { get; set; }
 
         public Dictionary<Ratio, double> Ratios { get; set; } = new Dictionary<Ratio, double>();
-        public void AddRatio(Attribute attribute, double value)
-        {
-            Ratios[attribute] = value;
-        }
-
-        public double GetRatio(Attribute attribute)
-        {
-            if (Ratios.TryGetValue(attribute, out double value))
-            {
-                return value;
-            }
-            else
-            {
-                throw new ArgumentException($"Ratio '{attribute}' not found for this stock.");
-            }
-        }
     }
 
     public enum Ratio
